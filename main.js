6e01a623a5a2df52ce1a9f5976bfea2e0a0ff1d4
Gabriel Lucas <gabriellucas2301@gmail.com>
2024-03-19 11:17:26 -0300

    Atualização

--- main.js
+++ main.js
@@ -14,8 +14,19 @@ const modalInfo = document.getElementById(
   "showModalInfo"
 );
 
+const modalBreve = document.getElementById(
+  "showModalBreve"
+);
+
 const closeModalInfo = document.getElementById(
   "closeModalInfo"
+);
+const closeModalBreve = document.getElementById(
+  "closeModalBreve"
+);
+
+const modalBreveClass = document.querySelector(
+  ".modal.modalBreve"
 );
 
 // Aqui abre o modal de Peças
@@ -40,5 +51,19 @@ closeModalInfo.addEventListener(
   "click",
   function () {
     modalInfoClass.classList.add("hidden");
+  }
+);
+
+// Aqui abre o modal de Em breve
+modalBreve.addEventListener("click", function () {
+  modalBreveClass.classList.remove("hidden");
+});
+
+// Fecha o Modal de Em breve
+
+closeModalBreve.addEventListener(
+  "click",
+  function () {
+    modalBreveClass.classList.add("hidden");
   }
 );
